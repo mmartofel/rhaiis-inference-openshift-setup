91d7a6f9432fdccfc14e0b338129f4a109e4e3cd
mmartofel <mmartofe@redhat.com>
2025-11-30 22:45:54 +0100

    model experimentation finished

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ echo
 echo "Starting Inference Model Service setup."
 echo
 
-cd granite-inference-service
+cd granite-inference-server
 ./preparation.sh
 ./namespace.sh.local
 ./docker-secret.sh.local
@@ -13,3 +13,11 @@ cd ..
 echo
 echo "Inference Model Service setup completed."
 echo
+
+# To monitor container downloading a model, use:
+#
+# oc get pods
+# oc logs -f granite-...... -c fetch-model
+#
+# fetch-model container will exit when the model is downloaded,
+# if any error occurs it will be shown in its logs like above.
